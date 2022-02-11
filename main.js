9797416868d1572f013e6ad701612d16268f0e16
Mary-fox <Mary-fox2008@yandex.ru>
2022-02-11 12:18:15 +0300

    написание операторов +,-,/,*

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function func() {
     let result;
     let number1 = Number(document.getElementById("number1").value);
     let number2 = Number(document.getElementById("number2").value);
-    switch (op) {
+    switch (operator) {
         case "+":
             result = number1 + number2;
             break;
