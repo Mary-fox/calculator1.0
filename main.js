770b06b2c4bf6f0d201580f727582c08ce736ce9
Mary-fox <Mary-fox2008@yandex.ru>
2022-02-15 17:15:07 +0300

    2 вариант через else if

--- main.js
+++ main.js
@@ -5,12 +5,13 @@
  * @param  {string} operator варианты операторов,которые обрабатываются при выборе одного из них
  * @returns {number} result выводит результат выбранного действия
  */
-let operator; //переменная,которая передает выбранное действие "+,-,*,/"
+/* let operator; //переменная,которая передает выбранное действие "+,-,*,/"
 
 function func() {
     let result; //переменная,которая передает результат
     let number1 = Number(document.getElementById("number1").value);
     let number2 = Number(document.getElementById("number2").value);
+
     switch (operator) {
         case "+":
             result = number1 + number2;
@@ -24,6 +25,28 @@ function func() {
         case "/":
             result = number1 / number2;
             break;
+    }
+    document.getElementById('result').innerHTML = result; //вывод результата на страницу
+}
+
+ */
+
+
+function calculator() {
+    let result;
+    let number1 = Number(document.getElementById("number1").value);
+    let number2 = Number(document.getElementById("number2").value);
+    let operator = String(document.getElementById("operator").value);
+    if (operator == "+") {
+        result = number1 + number2;
+    } else if (operator == "-") {
+        result = number1 - number2;
+    } else if (operator == "*") {
+        result = number1 * number2;
+    } else if (operator == "/") {
+        result = number1 / number2;
+    } else {
+        result = "ошибка";
     }
     document.getElementById('result').innerHTML = result; //вывод результата на страницу
 }
