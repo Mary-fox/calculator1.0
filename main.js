2d2231491bcdcc9ccfd16525e0fd089a7dba0402
Mary <89971822+Mary-fox@users.noreply.github.com>
2022-02-19 17:34:07 +0300

    lesson-2 [feature] Ограничения на ввод данных в инпут

--- main.js
+++ main.js
@@ -31,7 +31,6 @@ function func() {
 
  */
 
-
 function calc() {
     let result;
     let number1 = Number(document.getElementById("number1").value);
@@ -50,3 +49,11 @@ function calc() {
     }
     document.getElementById('result').innerHTML = result; //вывод результата на страницу
 }
+function control(input) { 
+    let value = input.value; 
+    let rep = /[\;":'а-яА-Я]/; 
+    if (rep.test(value)) { 
+        value = value.replace(rep, ''); 
+        input.value = value; 
+    } 
+} 
